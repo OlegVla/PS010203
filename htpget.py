4d48e000a8c0ab8fa00231e381c99fefc2d4e5c8
OlegVla <brezh1oleg@mail.ru>
2024-05-11 13:14:29 +0300

    add  file htpget

---
+++ htpget.py
@@ -0,0 +1,11 @@
+import requests
+import pprint
+url = "https://jsonplaceholder.typicode.com/posts"
+params ={
+    "userId": "1"
+}
+response = requests.get(url, params=params)
+print("Статус = ", response.status_code)
+
+response_json = response.json()
+pprint.pprint(response_json)
